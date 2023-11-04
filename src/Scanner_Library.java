fe624ac4a541e35cc2ccccd64161c9603b61939b
saiku <uduthasaikumar350@gmail.com>
2023-11-04 18:55:53 +0530

    Password Cipher

--- src/Scanner_Library.java
+++ src/Scanner_Library.java
@@ -1,3 +1,19 @@
-public class Scanner_Library {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.*;
 
+public class Scanner_Library {
+    public static List<Integer> scanValue(){
+        Scanner scan = new Scanner(System.in);
+        List<Integer> newList = new ArrayList<>(10);
+        for(int i=0;i<10;i++){
+            newList.add(scan.nextInt());
+        }
+        return newList;
+    }
+}
+class ScannerMain{
+    public static void main(String[] args){
+        System.out.println(Scanner_Library.scanValue());
+    }
 }
