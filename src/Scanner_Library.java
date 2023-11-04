6e2bc9042c950c608970f06f01f8bddf32f565a0
saiku <uduthasaikumar350@gmail.com>
2023-11-04 20:02:54 +0530

    Using Scanner to read the values in  Order.

--- src/Scanner_Library.java
+++ src/Scanner_Library.java
@@ -6,7 +6,7 @@ public class Scanner_Library {
     public static List<Integer> scanValue(){
         Scanner scan = new Scanner(System.in);
         List<Integer> newList = new ArrayList<>(10);
-        for(int i=0;i<10;i++){
+        for(int i=0;i<10;i++) {
             newList.add(scan.nextInt());
         }
         return newList;
